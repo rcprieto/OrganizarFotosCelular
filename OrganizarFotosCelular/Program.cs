4f818e9ed8f11e12e702c7d7a4b5be5484899c92
Rodrigo Prieto <rcprieto@gmail.com>
2022-01-19 08:33:18 -0300

    Add project files.

---
+++ OrganizarFotosCelular/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+RodarTratamento();
+
+static void RodarTratamento()
+{
+    Console.WriteLine("Informe a pasta dos arquivos");
+    string pasta = Console.ReadLine();
+    
+    DirectoryInfo dir = new DirectoryInfo(pasta);
+    var arquivos = dir.GetFiles();  
+    foreach (var arquivo in arquivos)
+    {
+        int mes = arquivo.LastWriteTime.Month;
+        int ano = arquivo.LastWriteTime.Year;
+
+        DirectoryInfo dirNovo = new DirectoryInfo($@"{pasta}/{ano}/{mes}/");
+        if(!dirNovo.Exists)
+            dirNovo.Create();
+
+        arquivo.MoveTo(dirNovo.FullName + arquivo.Name);
+    }
+    
+
+}
